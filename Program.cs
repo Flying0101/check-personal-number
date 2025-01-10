6855ade48202e0b8988a98a4458c9cc21d921a3e
Stephano <stephano@live.se>
2025-01-10 11:36:44 +0100

    add: project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace check_personal_nr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
