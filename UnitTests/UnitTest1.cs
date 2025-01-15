22f5c4f07332a31b341764003c268eb0d28e0a79
Sebastian Lindén <sebastian.linden@learnet.se>
2025-01-15 13:38:22 +0100

    add/fix: tests for new method/changed method

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using check_personal_nr;
+using check_personal_nr.Application;
 using Xunit;
 
 namespace UnitTest
@@ -8,86 +9,67 @@ namespace UnitTest
         [Fact]
         public void TestLengthYes()
         {
-
-
-            string testString = "0009302561";
-
+            string testString = "0009302561";         
             bool expectedResult = true;
-
-            bool actualResult = LenghtChecker.NumberandLenghtChecker(testString);
-
+            bool actualResult = LenghtChecker.NumberandLengthChecker(testString);
             Assert.Equal(expectedResult, actualResult);
         }
-
         [Fact]
         public void TestLengthNo()
         {
-
-
-            string testString = "000930256145";
-
+            string testString = "00093025614";
             bool expectedResult = false;
-
-            bool actualResult = LenghtChecker.NumberandLenghtChecker(testString);
-
+            bool actualResult = LenghtChecker.NumberandLengthChecker(testString);
             Assert.Equal(expectedResult, actualResult);
         }
-
         [Fact]
         public void TestOnlyDigitsYes()
         {
-
-
             string testString = "0009307878";
-
             bool expectedResult = true;
-
-            bool actualResult = LenghtChecker.NumberandLenghtChecker(testString);
-
+            bool actualResult = LenghtChecker.NumberandLengthChecker(testString);
             Assert.Equal(expectedResult, actualResult);
         }
-
-
         [Fact]
         public void TestOnlyDigitsYNo()
         {
-
-
             string testString = "ölkorv6969";
-
             bool expectedResult = false;
-
-            bool actualResult = LenghtChecker.NumberandLenghtChecker(testString);
-
+            bool actualResult = LenghtChecker.NumberandLengthChecker(testString);
             Assert.Equal(expectedResult, actualResult);
         }
-
-
         [Fact]
         public void TestTwelveMonthsYes()
         {
-
             string teststring = "0009302918";
             int[] testArray = {0, 0, 0, 9, 3, 0, 2, 9, 4, 5};
-
             bool expectedResult = true;
-
-            bool actualResult = DateChecker.DateofBirth(teststring, testArray);
-
+            bool actualResult = DateChecker.DateofBirth(testArray);
             Assert.Equal(expectedResult, actualResult);
         }
-
         [Fact]
         public void TestTwelveMonthsNo()
         {
-
             string teststring = "0014302918";
             int[] testArray = { 0, 0, 1, 4, 3, 0, 2, 9, 4, 5 };
-
             bool expectedResult = false;
-
-            bool actualResult = DateChecker.DateofBirth(teststring, testArray);
-
+            bool actualResult = DateChecker.DateofBirth(testArray);
+            Assert.Equal(expectedResult, actualResult);
+        }
+        [Fact]
+        public void TestControlNumberYes()
+        {
+            string testString = "0009302910";
+            bool expectedResult = true;
+            bool actualResult = ControlNumber.ConrtolCheckNumber(testString);
+            Assert.Equal(expectedResult, actualResult);
+        }
+        [Fact]
+        public void TestControlNumberNo()
+        {
+            string testString = "0009302917";
+            bool expectedResult = false;
+            bool actualResult = ControlNumber.ConrtolCheckNumber(testString);
             Assert.Equal(expectedResult, actualResult);
         }
     }
