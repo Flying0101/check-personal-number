0c31779d92b650714446486c96fd8f0b1836e4cc
BaselElOmar <Elomarbasel@gmail.com>
2025-01-15 11:25:33 +0100

    add: DateChecker class and mehtod

---
+++ Application/DateChecker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace check_personal_nr
+{
+    public class DateChecker 
+    {
+        
+
+        public static bool DateofBirth(string personNumber, int[] numberArray)
+        {
+            IsValid isValid = new IsValid();
+
+            int month = numberArray[2] * 10 + numberArray[3];
+            if (month < 0 || month > 12)
+            {
+                return false;
+            }
+
+            int day = numberArray[4] * 10 + numberArray[5];
+            if (day < 0 || day > 31)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
