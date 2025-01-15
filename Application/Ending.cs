4a22e964bb830747cef59e17e5ab5c212571fe0e
BaselElOmar <Elomarbasel@gmail.com>
2025-01-15 11:25:56 +0100

    

---
+++ Application/Ending.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace check_personal_nr
+{
+    public static class Ending
+    {
+        public static void TheEnd() 
+        { 
+            Console.Clear();
+            Console.WriteLine("Personal number accepted...");
+            Console.WriteLine("Stealing your identity...");
+            Thread.Sleep(1500);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@"""
+.-._                                                   _,-,
+  `._`-._                                           _,-'_,'
+     `._ `-._                                   _,-' _,'
+        `._  `-._        __.-----.__        _,-'  _,'
+           `._   `#===""""""           """"""===#'   _,'
+              `._/)  ._               _.  (\_,'
+               )*'     **.__     __.**     '*( 
+               #  .==..__  """"   """"  __..==,  # 
+               #   `""._(_).       .(_)_.""'   #
+""");
+
+            Thread.Sleep(2000);
+            ExitMove.Exit();
+        }
+    }
+}
+
