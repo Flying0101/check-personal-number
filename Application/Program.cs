18910362c4f4d61bf6637263fc12fdee5e35cea3
OlliCupitt <ollicupitt1999@gmail.com>
2025-01-15 13:05:14 +0100

    Modified: called upon ConrtolCheckNumber, comments

--- Application/Program.cs
+++ Application/Program.cs
@@ -7,20 +7,20 @@ namespace check_personal_nr
         {
             string personNumber = " ";
 
-            do
+            do                                                         // initial do while loop, if all the check bools are not correct the do while loop replays until a correct answe  is given
             {
-                Console.WriteLine("Input your personal number below. (10 digits)");
+                Console.WriteLine("Input your personal number below. (10 or 12 digits)");
                 Console.WriteLine("YYMMDDXXXX");
                 personNumber = Console.ReadLine();
                 
-                if (!LenghtChecker.NumberandLenghtChecker(personNumber) || !IsValid.IsValidPersonalNumber(personNumber))
+                if (!LenghtChecker.NumberandLengthChecker(personNumber) || !IsValid.IsValidPersonalNumber(personNumber))
                 {
                     Console.WriteLine("Invalid Person number. Please try again!");
                 }
                 
             }
 
-            while (!LenghtChecker.NumberandLenghtChecker(personNumber) || !IsValid.IsValidPersonalNumber(personNumber));
+            while (!LenghtChecker.NumberandLengthChecker(personNumber) || !IsValid.IsValidPersonalNumber(personNumber));
 
             Ending.TheEnd();
         }
